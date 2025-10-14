6e37cc869de23512845061e20dfc1b6741dac2fd
Yann <yann.kerverdo@gmail.com>
2025-10-14 18:08:10 +0200

    training with trainer

--- BASELINE/baseline.py
+++ BASELINE/baseline.py
@@ -1,14 +1,26 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
+from tqdm.auto import tqdm
+import numpy as np
+import collections
+import evaluate
+from transformers import TrainingArguments, Trainer, AutoModelForQuestionAnswering
 
 raw_datasets = load_dataset("squad")
 
 model_checkpoint = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+metric = evaluate.load("squad")
 
 
+n_best = 20
+max_answer_length = 30
 max_length = 384
 stride = 128
+
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
 
 
 def preprocess_training_examples(examples):
@@ -107,5 +119,76 @@ validation_dataset = raw_datasets["validation"].map(
     remove_columns=raw_datasets["validation"].column_names,
 )
 
-train_dataset.save_to_disk("./train_tokenized")
-validation_dataset.save_to_disk("./validation_tokenized")
+def compute_metrics(start_logits, end_logits, features, examples):
+    example_to_features = collections.defaultdict(list)
+    for idx, feature in enumerate(features):
+        example_to_features[feature["example_id"]].append(idx)
+
+    predicted_answers = []
+    for example in tqdm(examples):
+        example_id = example["id"]
+        context = example["context"]
+        answers = []
+
+        # Loop through all features associated with that example
+        for feature_index in example_to_features[example_id]:
+            start_logit = start_logits[feature_index]
+            end_logit = end_logits[feature_index]
+            offsets = features[feature_index]["offset_mapping"]
+
+            start_indexes = np.argsort(start_logit)[-1 : -n_best - 1 : -1].tolist()
+            end_indexes = np.argsort(end_logit)[-1 : -n_best - 1 : -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    # Skip answers that are not fully in the context
+                    if offsets[start_index] is None or offsets[end_index] is None:
+                        continue
+                    # Skip answers with a length that is either < 0 or > max_answer_length
+                    if (
+                        end_index < start_index
+                        or end_index - start_index + 1 > max_answer_length
+                    ):
+                        continue
+
+                    answer = {
+                        "text": context[offsets[start_index][0] : offsets[end_index][1]],
+                        "logit_score": start_logit[start_index] + end_logit[end_index],
+                    }
+                    answers.append(answer)
+
+        # Select the answer with the best score
+        if len(answers) > 0:
+            best_answer = max(answers, key=lambda x: x["logit_score"])
+            predicted_answers.append(
+                {"id": example_id, "prediction_text": best_answer["text"]}
+            )
+        else:
+            predicted_answers.append({"id": example_id, "prediction_text": ""})
+
+    theoretical_answers = [{"id": ex["id"], "answers": ex["answers"]} for ex in examples]
+    return metric.compute(predictions=predicted_answers, references=theoretical_answers)
+
+model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
+
+args = TrainingArguments(
+    "bert-finetuned-squad",
+    save_strategy="epoch",
+    learning_rate=3e-5,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    fp16=True,
+    push_to_hub=False,
+)
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    eval_dataset=validation_dataset,
+    tokenizer=tokenizer,
+)
+trainer.train()
+
+predictions, _, _ = trainer.predict(validation_dataset)
+start_logits, end_logits = predictions
+compute_metrics(start_logits, end_logits, validation_dataset, raw_datasets["validation"])
